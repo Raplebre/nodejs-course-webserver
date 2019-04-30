b6141beb8ca6ba74f2df43ea69a9f4464292c92c
Ruben Lebre <ruben.lebre@gmail.com>
2019-04-30 09:39:02 +0100

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Ficheiro javascript em client side carregado')
-
 /* fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data)
